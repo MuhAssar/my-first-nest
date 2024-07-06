845fa2b393519318b2da32b5ef11d4060e07bc3a
Muhamad Assar <m.assar@outlook.com>
2024-07-06 15:55:19 +0300

    تغيير رسالة الترحيب

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'السلام عليكم ورحمة الله وبركاته';
+    return 'مرحبًا بكم في لقاء ال mean stack';
   }
 }
